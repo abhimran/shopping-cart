f83be415cd7955924db4fc55de7f940c71aabb92
Abdullah Imran <abhimrannn@gmail.com>
2020-07-23 12:31:24 +0600

    add dynamic function check and checkout and removeItem

--- script.js
+++ script.js
@@ -1,145 +1,144 @@
+let updateSubTotal;
+let updateTaxNum;
+
+//Plus Buttton
+function increasePlus(id) {
+  let inputStr = document.getElementById(id).value;
+  let inputNum = parseInt(inputStr);
+  let PlusInputVal = inputNum + 1;
+  document.getElementById(id).value = PlusInputVal;
+}
+// product value
+function CustomizeProductval(id, ProductVal) {
+  let firstStr = document.getElementById(id).innerText;
+  let firstNum = parseInt(firstStr);
+  let totalVal = firstNum + ProductVal;
+  document.getElementById(id).innerText = totalVal;
+}
+// sub total
+function CustomizeSubTotal(id, ProductVal) {
+  let subTotal = document.getElementById(id).innerText;
+  let subTotalNum = parseInt(subTotal);
+  updateSubTotal = subTotalNum + ProductVal;
+  document.getElementById(id).innerText = updateSubTotal;
+}
+// Extra tax
+function CustomizeExtraTex(id, textAmount) {
+  let tax = document.getElementById(id).innerText;
+  let taxNum = parseInt(tax);
+  updateTaxNum = taxNum + textAmount;
+  document.getElementById(id).innerText = updateTaxNum;
+}
+// Total
+function fixTotal(id) {
+  let updateTotal = updateSubTotal + updateTaxNum;
+  document.getElementById(id).innerText = updateTotal;
+}
+
 // Product 1 Plus Buttton
 
-document.getElementById('plusBtn').addEventListener('click', function () {
+document.getElementById("plusBtn").addEventListener("click", function () {
+  // Increase Plus value
+  increasePlus("input1");
 
-    // Increase Plus value
-    let inputStr = document.getElementById('input1').value;
-    let inputNum = parseInt(inputStr);
-    let PlusInputVal = inputNum + 1;
-    document.getElementById('input1').value = PlusInputVal;
+  //Increase product value
+  CustomizeProductval("firstVal", 1219);
 
-    //Increase product value
-    let firstStr = document.getElementById('firstVal').innerText;
-    let firstNum = parseInt(firstStr);
-    let totalVal = firstNum + 1219;
-    document.getElementById('firstVal').innerText = totalVal;
+  // Update sub total
+  CustomizeSubTotal("subTotal", 1219);
 
-    // Update sub total
-    let subTotal = document.getElementById('subTotal').innerText;
-    let subTotalNum = parseInt(subTotal);
-    let updateSubTotal = subTotalNum +  1219;
-    document.getElementById('subTotal').innerText = updateSubTotal;
+  // Add Extra tax
+  CustomizeExtraTex("tax", 4);
 
-    // Add Extra tax
-    let tax = document.getElementById('tax').innerText;
-    let taxNum = parseInt(tax);
-    let updateTaxNum = taxNum +4;
-    document.getElementById('tax').innerText = updateTaxNum;
-
-    // Update total
-    let total = document.getElementById('total').innerText;
-    let totalNum = parseInt(total);
-    let updateTotal = updateSubTotal + updateTaxNum;
-    document.getElementById('total').innerText = updateTotal;
-})
+  //Update total
+  fixTotal("total");
+});
 
 // Product 2 Plus Button
 
-document.getElementById('plusBtn1').addEventListener('click', function () {
+document.getElementById("plusBtn1").addEventListener("click", function () {
+  // Increase Plus value
+  increasePlus("input2");
 
- // Increase Plus value
-    let inputStr1 = document.getElementById('input2').value;
-    let inputNum1 = parseInt(inputStr1);
-    let PlusInputVal1 = inputNum1 + 1;
-    document.getElementById('input2').value = PlusInputVal1;
+  //Increase product value
+  CustomizeProductval("secondVal", 59);
 
-    //Increase product value
-    let firstStr1 = document.getElementById('secondVal').innerText;
-    let firstNum1 = parseInt(firstStr1);
-    let totalVal1 = firstNum1 + 59;
-    document.getElementById('secondVal').innerText = totalVal1;
+  // Update sub total
+  CustomizeSubTotal("subTotal", 59);
+
+  // Add Extra tax
+  CustomizeExtraTex("tax", 1);
+
+  // Update total
+  fixTotal("total");
+});
+
+// Product 1 minus Button
+
+document.getElementById("minusBtn").addEventListener("click", function () {
+  let inputStr = document.getElementById("input1").value;
+  let inputNum = parseInt(inputStr);
+  if (inputNum > 1) {
+    let MinusInputVal = inputNum - 1;
+    document.getElementById("input1").value = MinusInputVal;
+
+    //Decrease product value
+    CustomizeProductval("firstVal", -1219);
 
     // Update sub total
-    let subTotal = document.getElementById('subTotal').innerText;
-    let subTotalNum = parseInt(subTotal);
-    let updateSubTotal = subTotalNum + 59;
-    document.getElementById('subTotal').innerText = updateSubTotal;
+    CustomizeSubTotal("subTotal", -1219);
 
-    // Add Extra tax
-    let tax = document.getElementById('tax').innerText;
-    let taxNum = parseInt(tax);
-    let updateTaxNum = taxNum + 1;
-    document.getElementById('tax').innerText = updateTaxNum;
+    // Remove Extra tax
+    CustomizeExtraTex("tax", -4);
 
     // Update total
-    let total = document.getElementById('total').innerText;
-    let totalNum = parseInt(total);
-    let updateTotal = updateSubTotal + updateTaxNum;
-    document.getElementById('total').innerText = updateTotal;
-
-})
-
-// Product 1 minus Button 
-
-document.getElementById('minusBtn').addEventListener('click', function () {
-    let inputStr = document.getElementById('input1').value;
-    let inputNum = parseInt(inputStr);
-    if (inputNum > 1) {
-        let MinusInputVal = inputNum - 1;
-        document.getElementById('input1').value = MinusInputVal;
-
-        //Decrease product value
-        let firstStr = document.getElementById('firstVal').innerText;
-        let firstNum = parseInt(firstStr);
-        let totalVal = firstNum - 1219;
-        document.getElementById('firstVal').innerText = totalVal;
-
-        // Update sub total
-        let subTotal = document.getElementById('subTotal').innerText;
-        let subTotalNum = parseInt(subTotal);
-        let updateSubTotal = subTotalNum - 1219;
-        document.getElementById('subTotal').innerText = updateSubTotal;
-
-        // Remove Extra tax
-        let tax = document.getElementById('tax').innerText;
-        let taxNum = parseInt(tax);
-        let updateTaxNum = taxNum - 4;
-        document.getElementById('tax').innerText = updateTaxNum;
-
-        // Update total
-        let total = document.getElementById('total').innerText;
-        let totalNum = parseInt(total);
-        let updateTotal = updateSubTotal + updateTaxNum;
-        document.getElementById('total').innerText = updateTotal;
-    }
-
-
-})
+    fixTotal("total");
+  }
+});
 
 // Product 2 minus Button
-document.getElementById('minusBtn1').addEventListener('click', function () {
+document.getElementById("minusBtn1").addEventListener("click", function () {
+  let inputStr1 = document.getElementById("input2").value;
+  let inputNum1 = parseInt(inputStr1);
+  if (inputNum1 > 1) {
+    let PlusInputVal1 = inputNum1 - 1;
+    document.getElementById("input2").value = PlusInputVal1;
 
-    let inputStr1 = document.getElementById('input2').value;
-    let inputNum1 = parseInt(inputStr1);
-    if (inputNum1 > 1) {
-        let PlusInputVal1 = inputNum1 - 1;
-        document.getElementById('input2').value = PlusInputVal1;
+    //Decrease product value
+    CustomizeProductval("secondVal", -59);
 
-         //Decrease product value
-        let firstStr1 = document.getElementById('secondVal').innerText;
-        let firstNum1 = parseInt(firstStr1);
-        let totalVal1 = firstNum1 - 59;
-        document.getElementById('secondVal').innerText = totalVal1;
+    // Update sub total
+    CustomizeSubTotal("subTotal", -59);
 
-        // Update sub total
-        let subTotal = document.getElementById('subTotal').innerText;
-        let subTotalNum = parseInt(subTotal);
-        let updateSubTotal = subTotalNum - 59;
-        document.getElementById('subTotal').innerText = updateSubTotal;
+    // Remove Extra tax
+    CustomizeExtraTex("tax", -1);
 
-        // Remove Extra tax
-        let tax = document.getElementById('tax').innerText;
-        let taxNum = parseInt(tax);
-        let updateTaxNum = taxNum - 1;
-        document.getElementById('tax').innerText = updateTaxNum;
+    // Update total
+    fixTotal("total");
+  }
+});
 
-        // Update total
-        let total = document.getElementById('total').innerText;
-        let totalNum = parseInt(total);
-        let updateTotal = updateSubTotal + updateTaxNum;
-        document.getElementById('total').innerText = updateTotal;
-    }
-})
+// Remove Item
+document.getElementById("removeItem").addEventListener("click", function () {
+  let removeItem = document.getElementById("cartItem");
+  removeItem.style.display = "none";
+  CustomizeSubTotal("subTotal", -1219);
+  CustomizeExtraTex("tax", -4);
+  fixTotal("total");
+});
+document.getElementById("removeItem2").addEventListener("click", function () {
+  let removeItem = document.getElementById("cartItem2");
+  removeItem.style.display = "none";
+  CustomizeSubTotal("subTotal", -59);
+  CustomizeExtraTex("tax", -1);
+  fixTotal("total");
+});
 
+// checkout
 
-// 
+document.getElementById("checkout").addEventListener("click", function () {
+  let cart = document.getElementById("cart");
+  cart.style.display = "none";
+  let checkoutSection = document.getElementById("checkoutSection");
+  checkoutSection.style.display = "block";
+});
